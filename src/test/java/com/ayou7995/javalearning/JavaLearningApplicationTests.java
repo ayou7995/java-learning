ef2a913f98ef03fd29ea51f9a690dcf2dbf3ae9f
Che Yu Lin <cheyu.lin@hulu.com>
2024-01-28 15:33:54 -0800

    initialize

---
+++ src/test/java/com/ayou7995/javalearning/JavaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayou7995.javalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
